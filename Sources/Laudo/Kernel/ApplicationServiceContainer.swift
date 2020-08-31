c8b92e34cb5e97fe23298a4965dfd6b9da6cb4c9
Alexander Zhuchkov <lexxanderdream@gmail.com>
2020-08-31 12:16:06 +0300

    app service container

---
+++ Sources/Laudo/Kernel/ApplicationServiceContainer.swift
@@ -0,0 +1,32 @@
+//
+//  ApplicationServiceContainer.swift
+//  Laudo
+//
+//  Created by Alexander Zhuchkov on 31.08.2020.
+//
+
+import Swinject
+
+protocol ApplicationServiceContainer {
+    var container: Container { get }
+    func resolve<Service>(_ serviceType: Service.Type) -> Service
+    func resolve<Service>(_ serviceType: Service.Type, name: String?) -> Service
+}
+
+extension ApplicationServiceContainer {
+    func registerServices(providers: [ServiceProvider.Type]) {
+        providers.forEach { (providerType) in
+            let provider = providerType.init(container: container)
+            print("Register service", providerType)
+            provider.register()
+        }
+    }
+    
+    func resolve<Service>(_ serviceType: Service.Type) -> Service {
+        return container.resolve(serviceType)!
+    }
+    
+    func resolve<Service>(_ serviceType: Service.Type, name: String?) -> Service {
+        return container.resolve(serviceType, name: name)!
+    }
+}
